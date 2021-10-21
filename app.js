efe4c3468e6bf52c183020bc7c1fcc14c1a9520f
Melvin Welton <nivlem65@gmail.com>
2021-10-21 11:42:17 -0500

    commit to branch

--- app.js
+++ app.js
@@ -1,24 +1,45 @@
-//console.log('Hello Node!');
-//console.log(document)
-var message ='Hello Node!';
-var sum = 5 + 3;
-console.log(message);
-console.log(sum);
-var commandLineArgs = process.argv;
-console.log(commandLineArgs);
-var profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+
+//comments
+//var message ='Hello Node!';
+//var sum = 5 + 3;
+//console.log(message);
+//console.log(sum);
+
+//var commandLineArgs = process.argv;
+//console.log(commandLineArgs);
+//var profileDataArgs = process.argv.slice(2, process.argv.length);
+//console.log(profileDataArgs);
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 console.log(profileDataArgs);
 const printProfileData = (profileDataArr) => {
     console.log(profileDataArr);
 };
 
-printProfileData(profileDataArgs);
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i++){
+const name = profileDataArgs [0];
+const github = profileDataArgs[1];
 
-    }
-}
+const generatePage = () => 'Name: Jane, Github: janehub';
+console.log(generatePage(name, github));
+console.log(generatePage('Jane', 'janehub'));
+
+const generatePage = (userName, githubName) => {
+    return `
+    Name: ${userName}
+    GitHub: ${githubName}
+    `;
+};
+
+//printProfileData(profileDataArgs);
+//const printProfileData = profileDataArr => {
+   // for (let i = 0; i < profileDataArr.length; i+= 1){
+       // console.log(profileDataArr [i]);
+
+    //}
+   // console.log('===');
+
+//}
+
+//profileDataArr.forEach(profileItem => console.log(profileItem));
 
 
