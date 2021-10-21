eb17c2d0888f1d37c69d9bff9735206c1fe6221b
Melvin Welton <nivlem65@gmail.com>
2021-10-20 22:01:58 -0500

    commit to branch

--- app.js
+++ app.js
@@ -22,11 +22,3 @@ const printProfileData = profileDataArr => {
 }
 
 
-//most recent code from befor
-//const printProfileData = profileDataArr => {
-   // for (let i = 0; i < profileDataArr.length; i += 1) {
-        //console.log(profileDataArr[i]);
-    //}
-   // console.log('==========');
-//}
-//profileDataArr.forEach((profileItem => console.log(profileItem))
