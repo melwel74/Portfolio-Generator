3a3d9c7090ceef2a6493ea26fc3033179701627d
melvin welton <88204914+melwel74@users.noreply.github.com>
2021-10-24 18:29:50 -0500

    Update app.js

--- app.js
+++ app.js
@@ -5,10 +5,10 @@
 //console.log(message);
 //console.log(sum);
 
-//var commandLineArgs = process.argv;
-//console.log(commandLineArgs);
-//var profileDataArgs = process.argv.slice(2, process.argv.length);
-//console.log(profileDataArgs);
+var commandLineArgs = process.argv;
+console.log(commandLineArgs);
+var profileDataArgs = process.argv.slice(2, process.argv.length);
+console.log(profileDataArgs);
 
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 console.log(profileDataArgs);
@@ -30,16 +30,16 @@ const generatePage = (userName, githubName) => {
     `;
 };
 
-//printProfileData(profileDataArgs);
-//const printProfileData = profileDataArr => {
-   // for (let i = 0; i < profileDataArr.length; i+= 1){
-       // console.log(profileDataArr [i]);
+printProfileData(profileDataArgs);
+const printProfileData = profileDataArr => {
+   for (let i = 0; i < profileDataArr.length; i+= 1){
+       console.log(profileDataArr [i]);
 
-    //}
-   // console.log('===');
+    }
+   console.log('===');
 
-//}
+}
 
-//profileDataArr.forEach(profileItem => console.log(profileItem));
+profileDataArr.forEach(profileItem => console.log(profileItem));
 
 
