cc0eb1ccb9020e9078715ac837db21d0ab3fc92c
Melvin Welton <nivlem65@gmail.com>
2021-10-25 19:18:45 -0500

    commit to main

--- app.js
+++ app.js
@@ -1,28 +1,13 @@
 
-
-
-
-
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 console.log(profileDataArgs);
 
-
-// const name = profileDataArgs[0];
-// const github = profileDataArgs[1];
 const [Melvin, melwel74] = profileDataArgs;
 console.log(Melvin, melwel74);
 console.log(generatePage(Melvin, melwel74));
 
-// const printProfileData = (profileDataArr) => {
-//     console.log(profileDataArr);
-// };
-
-// const name = profileDataArgs[0];
-// const github = profileDataArgs[1];
-//comment out everything above
-
 const generatePage = (Melvin, melwel74) => `Name: ${Melvin}, Github: ${melwel74}`;
-// console.log(generatePage(Melvin, melwel74));
+
 console.log(generatePage('Melvin', 'melwel74'));
 
 const generatePage = (Melvin, melwel74) => {
@@ -33,11 +18,8 @@ const generatePage = (Melvin, melwel74) => {
 };
 
 
-
-
-//cleaner loops
 profileDataArr.forEach(profileItem => console.log(profileItem));
-    
+
 
 
 
